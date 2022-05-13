eb671d3953a921e8a075ed7911e1a94b2dfe45cb
20181275 조준하 <junha0647@naver.com>
2022-05-14 07:21:56 +0900

    3차 브런치
    게임 화면에서 플레이어 캐릭터 wasd 이동 및 UI 대강 구성, 마우스를 바라보는 손전등 구현

---
+++ Home/Assets/3. Scripts/2. Game Scene/PlayerFlash.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFlash : MonoBehaviour
+{
+    void Update()
+    {
+        Vector2 len = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float z = Mathf.Atan2(len.y, len.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, z);
+    }
+}
