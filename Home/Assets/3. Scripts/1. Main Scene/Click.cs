937470e2e7847c46c52203bf0657f46bdc883f52
20181275 조준하 <junha0647@naver.com>
2022-05-15 16:28:11 +0900

    12차 정상대전
    이게 원피스다... 가슴이 웅장해진다...

--- Home/Assets/3. Scripts/1. Main Scene/Click.cs
+++ Home/Assets/3. Scripts/1. Main Scene/Click.cs
@@ -1,59 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 enum BTNType
 {
     Next,
-    //Restart,
-    //MainMenu,
-    //Back,
-    //Continue_Yes,
-    //Continue_No
 }
 
 public class Click : MonoBehaviour
 {
     [Tooltip("각 버튼의 기능 선택")]
     [SerializeField] private BTNType currentType;
-    //public UI_test uiTest;
-    //public GameObject crossHair;
-    //[Tooltip("ESC 누르면 나오는 오브젝트 그룹")]
-    //[SerializeField] private CanvasGroup optionGroup;
+
+    [SerializeField] private SoundManager soundManager;
+    int cnt = 0;
+
+    [SerializeField] private GameObject tuto1;
+    [SerializeField] private GameObject tuto2;
 
     public void OnBtnClick()
     {
+        soundManager.PlaySound("Enter");
+
         switch (currentType)
         {
             case BTNType.Next:
-                SceneManager.LoadScene("2. Game Scene");
+                ++cnt;
+                if(cnt == 1)
+                {
+                    tuto1.SetActive(false);
+                    tuto2.SetActive(true);
+                }
+                else
+                {
+                    StartCoroutine(gameStart());
+                }
                 break;
-            //case BTNType.Restart:
-            //    SceneManager.LoadScene(PlayerCtrl.stageNum + "stage");
-            //    PlayerCtrl.key = 0;
-            //    break;
-            //case BTNType.MainMenu:
-            //    SceneManager.LoadScene("Main Menu");
-            //    PlayerCtrl.stageNum = 1;
-            //    PlayerCtrl.key = 0;
-            //    break;
-            //case BTNType.Back:
-            //    uiTest.CanvasGroupOff(optionGroup);
-            //    Cursor.visible = false;
-            //    Cursor.lockState = CursorLockMode.Locked;
-            //    crossHair.SetActive(true);
-            //    uiTest.CameraOn();
-            //    break;
-            //case BTNType.Continue_Yes:
-            //    SceneManager.LoadScene(PlayerCtrl.stageNum + "stage");
-            //    PlayerCtrl.key = 0;
-            //    break;
-            //case BTNType.Continue_No:
-            //    SceneManager.LoadScene("Main Menu");
-            //    PlayerCtrl.stageNum = 1;
-            //    PlayerCtrl.key = 0;
-            //    break;
         }
+    }
+
+    IEnumerator gameStart()
+    {
+        yield return new WaitForSeconds(1.5f);
+        SceneManager.LoadScene("2. Game Scene");
     }
 }
